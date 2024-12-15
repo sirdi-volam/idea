67993deef0c951e1f36d7136ce435c9a983c8544
sirdi <sirdi-volam@gmail.com>
2024-12-15 18:19:57 +0300

    Добавлен prettier, убраны лишние файлы

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import './App.css'
-
-function App() {
-
+export const App = () => {
+  const ideas = [
+    {
+      nick: 'cool-idea-nick-1',
+      name: 'Idea 1',
+      description: 'Description of idea 1...',
+    },
+    {
+      nick: 'cool-idea-nick-2',
+      name: 'Idea 2',
+      description: 'Description of idea 2...',
+    },
+    {
+      nick: 'cool-idea-nick-3',
+      name: 'Idea 3',
+      description: 'Description of idea 3...',
+    },
+    {
+      nick: 'cool-idea-nick-4',
+      name: 'Idea 4',
+      description: 'Description of idea 4...',
+    },
+    {
+      nick: 'cool-idea-nick-5',
+      name: 'Idea 5',
+      description: 'Description of idea 5...',
+    },
+  ];
   return (
-    <>
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>Ideanick</h1>
+      {ideas.map((idea) => (
+        <div key={idea.nick}>
+          <h2>{idea.name}</h2>
+          <p>{idea.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
